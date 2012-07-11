09f3462ef63f523d8bd7b7eaaabdbd992dd5ddc0
Kristian Nielsen <kristian_n@Kristians-MacBook-9.local>
2012-07-11 13:14:40 +0200

    Initial Commit

---
+++ KattealderTests/KattealderTests.h
@@ -0,0 +1,13 @@
+//
+//  KattealderTests.h
+//  KattealderTests
+//
+//  Created by Kristian Nielsen on 11/07/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface KattealderTests : SenTestCase
+
+@end
