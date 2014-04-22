51a7688ddb29d1ea6c7f4af3b5bba512e9fa951b
krisdk <kristian@apphaus.dk>
2014-04-22 09:32:15 +0200

    Første gang

--- Kattealder/ViewController.m
+++ Kattealder/ViewController.m
@@ -11,8 +11,11 @@
 @interface ViewController ()
 
 @end
-
+//Outlets defineret i headerfil (h-fil.
 @implementation ViewController
+@synthesize HumanAlder;
+@synthesize Katteaar;
+@synthesize SliderValue;
 
 - (void)viewDidLoad
 {
@@ -22,6 +25,9 @@
 
 - (void)viewDidUnload
 {
+    [self setHumanAlder:nil];
+    [self setKatteaar:nil];
+    [self setSliderValue:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
 }
@@ -31,4 +37,16 @@
     return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
 }
 
+- (IBAction)Alderskiftet:(id)sender {
+    float KatteAlderValue = [(UISlider *)sender value];
+    //float fordi det er fra "slider"
+    //tager værdi fra slider og sætter den i KatteAlderValue
+
+    
+    Katteaar.text = [NSString stringWithFormat:@"%d katteår", (int)KatteAlderValue];
+    // Tager værdi fra KatteAlderValue, konverterer den og viser den i Katteaar variabel.
+
+        HumanAlder.text = [NSString stringWithFormat:@"%d menneskeår", (int)KatteAlderValue*4];
+    
+}
 @end
